1ea6a2a8340e13f262a6a138f58d0aa237fb5525
CharlieYu <yuhangbinccc@gmail.com>
2024-09-02 23:07:19 +0800

    Initial Commit

---
+++ eggs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eggs
+//
+//  Created by 余杭斌 on 2024/9/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eggs/eggsApp.swift
@@ -0,0 +1,17 @@
+//
+//  eggsApp.swift
+//  eggs
+//
+//  Created by 余杭斌 on 2024/9/2.
+//
+
+import SwiftUI
+
+@main
+struct eggsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
